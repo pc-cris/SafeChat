29a272a921400e6c70a48ed0339fea6d53fc686c
Cristina Pocol <Cristina@192-168-0-102.rdsnet.ro>
2017-01-05 22:45:24 +0200

    Initial Commit

---
+++ SafeChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SafeChat
+//
+//  Created by Cristina Pocol on 05/01/2017.
+//  Copyright © 2017 Cristina Pocol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
